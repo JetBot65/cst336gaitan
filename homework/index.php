95497c601ba3322555069cac755c713e9e6490e8
Terry <tgaitan@csumb.edu>
2018-02-09 03:48:09 +0000

    first push

---
+++ homework/index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Homework </title>
+    </head>
+    <body>
+    This Is Homework 1
+    <div> <a href="/">BACK</a></div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+    <div>
+        <h1> Homework</h1>
+        <a href="/homework/1/hwk1">Homework 1 Link</a>
+       
+    </div>
+    </body>
+</html>
