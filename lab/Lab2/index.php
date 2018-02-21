f8ed19945ae75ca40fd2e15d3bf41d699689e054
Terry <tgaitan@csumb.edu>
2018-02-21 17:07:00 +0000

    last push

--- lab/Lab2/index.php
+++ lab/Lab2/index.php
@@ -2,12 +2,23 @@
 <html>
     <head>
         <title> 777 Slot Machine </title>
-        <link rel="stylesheet" type="text/css" href="./css/styles.css">
+        <link rel ="stylesheet" type="text/css" href="./css/styles.css">
         <style>
             @import url("css/styles.css");
         </style>
     </head>
-    <body>
-
-    </body>
+    
+     <body>
+         <div id="main">
+        <?php
+      
+         include 'inc/function.php';
+          play();
+         ?>
+         
+         <form>
+         <input type= "submit" value="Spin!"/>
+         </form>
+         </div>
+         </body>
 </html>
