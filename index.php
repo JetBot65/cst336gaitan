48c43f8ef29182c66f9fc0d573dc40add597f073
Terry <tgaitan@csumb.edu>
2018-03-16 19:17:20 +0000

    changes to lab 4

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <div>
         <h1> Homework and Labs</h1>
         <a href="/homework/1/hwk1">Homework 1 Link</a>
-         <a href="/lab/lab2/index.php">Lab2  Link</a>
+         <a href="/Lab/lab2/index.php">Lab2  Link</a>
          <a href="/homework/hwk2/index.php">Homework 2 Link</a>
-         <a href="/lab/lab4/index.php">Lab4 Link</a>
+         <a href="/Lab/LAB4/img/index.php">Lab4 Link</a>
     </div>
     </body>
 </html>
