1bf3d5cf5e04ec0b0a40cf73081a71fa46cd243f
Terry <tgaitan@csumb.edu>
2018-03-16 18:09:03 +0000

    add lab4 link to index php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <a href="/homework/1/hwk1">Homework 1 Link</a>
          <a href="/lab/Lab2/inc/index.php">Lab2  Link</a>
          <a href="/homework/hwk2/index.php">Homework 2  Link</a>
-       
+          <a href="/lab/lab4/index.php">Homework 2  Link</a>
     </div>
     </body>
 </html>
