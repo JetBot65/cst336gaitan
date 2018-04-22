617d333210d5b584408c760800614c44f42516fa
Terry <tgaitan@csumb.edu>
2018-04-22 02:00:55 +0000

    changed index.php file location

--- homework/1/index.php
+++ homework/1/index.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
 <html>
     <head>
         <title> </title>
     </head>
-    
+    <body>
     <div>
         <h1> Homework and Labs and Group Project</h1>
         <a href="/homework/1/hwk1">Homework 1 Link</a>
@@ -11,12 +10,7 @@
          <a href="/homework/hwk2/index.php">Homework 2 Link</a>
          <a href="/Lab/LAB4/index.php">Lab4 Link</a>
          <a href="/Lab/Lab5/index.php">Lab5 Link</a>
-         <a href="/homework/index.html">HWK link</a>
+		 <a href="/homework/hwk4/index.php">Homework 4 link</a>
     </div>
     </body>
 </html>
-     
-    <div></div>
-    </body>
-</html>
-        
